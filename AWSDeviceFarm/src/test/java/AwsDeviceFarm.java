cb462ce82f2ef3ef8ed05eff8ceebeb46c478497
Hirok Sarker <hirok.sarker@gmail.com>
2020-01-26 10:23:04 +0600

    AWS Device Farm Using Selenium WebDriver

---
+++ AWSDeviceFarm/src/test/java/AwsDeviceFarm.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.devicefarm.DeviceFarmClient;
+import software.amazon.awssdk.services.devicefarm.model.CreateTestGridUrlRequest;
+import software.amazon.awssdk.services.devicefarm.model.CreateTestGridUrlResponse;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AwsDeviceFarm {
+    private static RemoteWebDriver driver;
+
+    @BeforeTest
+    void setUp() throws MalformedURLException {
+
+        String myProjectARN = "YOUR PROJECT ARN";
+        DeviceFarmClient client  = DeviceFarmClient.builder()
+                .region(Region.US_WEST_2)
+                .build();
+        CreateTestGridUrlRequest request = CreateTestGridUrlRequest.builder()
+                .expiresInSeconds(300)
+                .projectArn(myProjectARN)
+                .build();
+        CreateTestGridUrlResponse response = client.createTestGridUrl(request);
+        URL testGridUrl = new URL(response.url());
+        driver = new RemoteWebDriver(testGridUrl, DesiredCapabilities.chrome());
+    }
+
+    @Test
+    public void userLogin()
+    {
+        driver.manage().window().maximize();
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
+    }
+
+    @AfterTest
+    void tearDown() {
+        driver.quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# aws-device-farm-maven
+# AWS Device Farm and Selenium WebDriver
 Device Farm Desktop Browser &amp; Mobile Testing
+
+AWS Device Farm is a testing service from AWS that can help us to improve the quality of our web and mobile applications by testing them across a wide range of desktop browsers and real mobile devices without the need to deploy and manage a test infrastructure. 
+
+The service allows us to run our tests on multiple desktop browsers or real devices to speed up the execution of your test suite and generates videos and logs to help you quickly identify problems with your application.
+
+About AWS Device Farm Details: https://docs.aws.amazon.com/devicefarm/latest/testgrid/what-is-testgrid.html
+Windows Configuration: https://docs.aws.amazon.com/cli/latest/userguide/install-windows.html
+AWS CLI Configuration: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
